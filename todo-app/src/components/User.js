84a39d8dfe3dbcb19edd7488356e49424de09429
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-08-25 16:36:56 +0330

    add css ,image

--- todo-app/src/components/User.js
+++ todo-app/src/components/User.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const User = () => {
-  return <div className="User">User</div>;
+  return <div className="User">
+    <div className="logo">
+      <h4>TODO</h4>
+    </div>
+    <div className="info">
+      <p>Code Explained</p>
+      <a href="#">Logout!</a>
+    </div>
+  </div>;
 };
 
 export default User;
