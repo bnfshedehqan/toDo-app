441c880ff9d621d44e22e4babc77f5e852e8ca59
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-08-27 00:32:44 +0330

    Add new todo is run

--- todo-app/src/components/AddNewTodo.js
+++ todo-app/src/components/AddNewTodo.js
@@ -1,7 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal from "./Modal";
 const AddNewTodo = () => {
-  return <div className="AddNewTodo">AddNewTodo</div>;
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className="AddNewTodo">
+      <div className="btn">
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          + New Todo
+        </button>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          Hello
+          <button
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            hide
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default AddNewTodo;
