9a5f52ec500b4cf26aa0f630191864cfc46089b8
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-09-02 01:54:43 +0330

    update calendar

--- todo-app/src/components/Calender.js
+++ todo-app/src/components/Calender.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { CalendarDate, CaretUp } from "react-bootstrap-icons";
+import { calendarItems } from "../constants/index";
 
-const Calender = () => {
-  return <div className="Calender">Calender</div>;
-};
+function Calender() {
+  return (
+    <div className="Calender">
+      <div className="header">
+        <div className="title">
+          <CalendarDate size={18} />
+          <p>Calender</p>
+        </div>
+        <div className="btns">
+          <span>
+            <CaretUp size={20} />
+          </span>
+        </div>
+      </div>
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div className="item" key={item}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Calender;
