5227fc6c8491aaa28ed5a4c3dd33cc9bb346ed53
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-09-05 02:38:47 +0330

    RenameProject is done

--- todo-app/src/components/RenameProject.js
+++ todo-app/src/components/RenameProject.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import ProjectForm from "./ProjectForm";
 
-const RenameProject = () => {
-  return <div className="RenameProject">RenameProject</div>;
+const RenameProject = (project, setShowModal) => {
+  const [newProjectName, setNewProjectName] = useState(project.name);
+
+  function handleSubmit(e) {}
+
+  return (
+    <div className="RenameProject">
+      <ProjectForm
+        handleSubmit={handleSubmit}
+        heading="Edit Project Name!"
+        value={newProjectName}
+        setValue={setNewProjectName}
+        setShowModal={setShowModal}
+        confirmButtonText="Confirm"
+      />
+    </div>
+  );
 };
 
 export default RenameProject;
