0017fb1f6b2cf5b386c340bd159979cdd6edb019
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-09-05 02:17:20 +0330

    ProjectForm is done

---
+++ todo-app/src/components/ProjectForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProjectForm = ({
+  handleSubmit,
+  heading,
+  value,
+  setValue,
+  setShowModal,
+  confirmButtonText,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>{heading}</h3>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder="project name..."
+        autoFocus
+      />
+      <button
+        className="cancel"
+        role="button"
+        onClick={() => setShowModal(false)}
+      >
+        Cancel
+      </button>
+      <button className="confirm">{confirmButtonText}</button>
+    </form>
+  );
+};
+
+export default ProjectForm;
