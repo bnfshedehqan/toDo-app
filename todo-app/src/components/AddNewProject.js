0b271849f3d0a181f0a314da79bb2b5e0b16d7c6
Banafshe Dehqan <64371848+bnfshedehqan@users.noreply.github.com>
2024-09-05 02:28:24 +0330

    AddNewProject is done

--- todo-app/src/components/AddNewProject.js
+++ todo-app/src/components/AddNewProject.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Plus } from "react-bootstrap-icons";
+import Modal from "./Modal";
+import ProjectForm from "./ProjectForm";
 
 const AddNewProject = () => {
-  return <div className="AddNewProject">
-    <div className="add-button">
-      <span>
-        <Plus size={20} />
-      </span>
+  const [showModal, setShowModal] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  function handleSubmit(e) {}
+  return (
+    <div className="AddNewProject">
+      <div className="add-button">
+        <span onClick={() => setShowModal(true)}>
+          <Plus size={20} />
+        </span>
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <ProjectForm
+          handleSubmit={handleSubmit}
+          heading="New Project"
+          value={projectName}
+          setShowModal={setShowModal}
+          confirmButtonText="+ Add Project"
+        />
+      </Modal>
     </div>
-  </div>;
+  );
 };
 
 export default AddNewProject;
